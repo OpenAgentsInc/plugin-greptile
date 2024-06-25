670819ad1bfc22090d648c50863d1d327c0b1d0a
Christopher David <chris@arcadelabs.co>
2024-06-25 16:25:44 -0500

    Successful greptile index call

--- Makefile
+++ Makefile
@@ -1,6 +1,17 @@
+# Load environment variables from .env file
+ifneq (,$(wildcard .env))
+    include .env
+    export
+endif
+
 build:
 	tinygo build -target wasi -o plugin.wasm main.go
 
 test:
-	extism call plugin.wasm greet --input "world" --wasi
+	@if [ -z "$(GREPTILE_API_KEY)" ] || [ -z "$(GITHUB_TOKEN)" ]; then \
+		echo "Error: GREPTILE_API_KEY and GITHUB_TOKEN must be set in .env file"; \
+		exit 1; \
+	fi
+	extism call plugin.wasm run --input '{"repository": "openagentsinc/kb-wanix", "remote": "github", "branch": "main", "api_key": "$(GREPTILE_API_KEY)", "github_token": "$(GITHUB_TOKEN)"}' --wasi --allow-host="*.greptile.com"
 
+.PHONY: build test
--- main.go
+++ main.go
@@ -1,76 +1,76 @@
+//go:build !std
+// +build !std
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 
 	"github.com/extism/go-pdk"
 )
 
 type input struct {
-	Text         string `json:"text"`
-	To           string `json:"to"`
-	ApiKey       string `json:"api_key"`
-	GithubToken  string `json:"github_token"`
+	Repository  string `json:"repository"`
+	Remote      string `json:"remote"`
+	Branch      string `json:"branch"`
+	ApiKey      string `json:"api_key"`
+	GithubToken string `json:"github_token"`
 }
 
-//go:export run
+//export run
 func run() int32 {
 	url := "https://api.greptile.com/v2/repositories"
-	payload := strings.NewReader(`{
-		"remote": "github",
-		"repository": "openagentsinc/kb-wanix",
-		"branch": "main",
-		"reload": true,
-		"notify": true
-	}`)
 
 	in := input{}
 	err := pdk.InputJSON(&in)
 	if err != nil {
-		pdk.SetError(err)
+		pdk.SetError(fmt.Errorf("failed to parse input JSON: %v", err))
 		return 1
 	}
 
-	req, err := http.NewRequest("POST", url, payload)
+	if in.Repository == "" {
+		pdk.SetError(fmt.Errorf("must specify repository"))
+		return 1
+	}
+
+	if in.Remote == "" {
+		in.Remote = "github"
+	}
+
+	if in.Branch == "" {
+		in.Branch = "main"
+	}
+
+	payload := map[string]interface{}{
+		"remote":     in.Remote,
+		"repository": in.Repository,
+		"branch":     in.Branch,
+		"reload":     true,
+		"notify":     true,
+	}
+
+	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
-		pdk.SetError(err)
+		pdk.SetError(fmt.Errorf("failed to marshal payload: %v", err))
 		return 1
 	}
 
-	req.Header.Add("Authorization", "Bearer "+in.ApiKey)
-	req.Header.Add("X-GitHub-Token", in.GithubToken)
-	req.Header.Add("Content-Type", "application/json")
+	req := pdk.NewHTTPRequest(pdk.MethodPost, url)
+	req.SetHeader("Authorization", fmt.Sprintf("Bearer %s", in.ApiKey))
+	req.SetHeader("X-GitHub-Token", in.GithubToken)
+	req.SetHeader("Content-Type", "application/json")
+	req.SetBody(payloadBytes)
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		pdk.SetError(err)
-		return 1
-	}
-	defer res.Body.Close()
+	res := req.Send()
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		pdk.SetError(err)
+	if res.Status() != 200 {
+		pdk.SetError(fmt.Errorf("failed to index repository: status %d", res.Status()))
 		return 1
 	}
 
-	fmt.Println(res)
-	fmt.Println(string(body))
-
-	pdk.OutputString("Attempting to index repository")
+	pdk.OutputString(fmt.Sprintf("Response status: %d\nBody: %s", res.Status(), string(res.Body())))
 	return 0
 }
 
-//go:export greet
-func Greet() int32 {
-	name := pdk.InputString()
-	pdk.OutputString("Hello, " + name)
-	return 0
-}
-
-func main() {
-	fmt.Println("Hello, World!")
-}
+func main() {}
